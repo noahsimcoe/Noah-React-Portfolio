7b12f52eec09010a32ead6a7095b08dac291ca0c
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-11-04 17:21:03 -0500

    added technology proficiencies to resume page

--- src/components/UI/Resume.jsx
+++ src/components/UI/Resume.jsx
@@ -8,6 +8,30 @@ export default function Resume() {
             <a href={resumeUrl} target="_blank" rel="noopener noreferrer" download>
                 Noah Simcoe - Resume (11.4.23)
             </a>
+            <div className="proficiency-box">
+                <h3>Front-end Proficiencies</h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>jQuery</li>
+                        <li>Responsive Design</li>
+                        <li>React</li>
+                        <li>Bootstrap</li>
+                    </ul>
+            </div>
+            <div className="proficiency-box">
+                <h3>Back-end Proficiencies</h3>
+                <ul>
+                        <li>APIs</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>MySQL, Sequelize</li>
+                        <li>MongoDB, Mongoose</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                    </ul>
+            </div>
         </div>
         </>
     )
