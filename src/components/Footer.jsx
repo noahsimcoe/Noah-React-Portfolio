19f286a3b72ce1bc68e03e2a6b0928244ae7e583
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-11-04 17:34:33 -0500

    trying to fix problem with netlify crashing app

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
         <>
             <div className="footer">
                 <a href="https://github.com/noahsimcoe" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faGithub} /> GitHub
+                    <FontAwesomeIcon icon={fab.faGithub} /> GitHub
                 </a>
                 <a href="https://www.linkedin.com/in/noah-s-26b8b11b2/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+                    <FontAwesomeIcon icon={fab.faLinkedin} /> LinkedIn
                 </a>
             </div>
         </>
